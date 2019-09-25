1fcc6caf1e48b3a64cca0b5942504630ad20aee4
sofiaruiz <srh_19@hotmail.com>
2019-09-24 20:18:51 -0500

    the form to save customers has been designed and the route has been made to send them to the database

---
+++ src/controllers/customerController.js
@@ -0,0 +1,20 @@
+const controller = {};
+
+controller.list = (req,res) => {
+    req.getConnection((err, conn) => {
+        conn.query('SELECT * FROM customer', (err, customers) => {
+            if(err) {
+                res.json(err);
+            }
+            res.render('customers', {
+                data: customers
+            });
+        });
+    });
+};
+
+controller.save = (req,res) => {
+
+};
+
+module.exports = controller;
--- src/routes/customer.js
+++ src/routes/customer.js
@@ -1,5 +1,11 @@
 const express = require('express');
 const router = express.Router();
 
+const customerController = require('../controllers/customerController');
+
+router.get('/', customerController.list);
+router.post('/add', customerController.save);
+
+
 
 module.exports = router;
